c2388934073fb83feea763ea350918594b5fc125
vegarfa <vegarfa@stud.ntnu.no>
2016-10-24 15:15:29 +0200

    fix weak password

--- src/webapp/validation/RegistrationFormValidation.php
+++ src/webapp/validation/RegistrationFormValidation.php
@@ -7,19 +7,19 @@ use tdt4237\webapp\models\User;
 class RegistrationFormValidation
 {
     const MIN_USER_LENGTH = 3;
-    
+
     private $validationErrors = [];
-    
+
     public function __construct($username, $password, $first_name, $last_name, $phone, $company)
     {
         return $this->validate($username, $password, $first_name, $last_name, $phone, $company);
     }
-    
+
     public function isGoodToGo()
     {
         return empty($this->validationErrors);
     }
-    
+
     public function getValidationErrors()
     {
         return $this->validationErrors;
@@ -27,8 +27,8 @@ class RegistrationFormValidation
 
     private function validate($username, $password, $first_name, $last_name, $phone, $company)
     {
-        if (empty($password)) {
-            $this->validationErrors[] = 'Password cannot be empty';
+        if (!preg_match('/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,24}$/', $password)) {
+            $this->validationErrors[] = 'Password must be minimum 8 and maximum 24 characters long, use uppercase, lowercase letters, numbers and special characters';
         }
 
         if(empty($first_name)) {
@@ -40,7 +40,7 @@ class RegistrationFormValidation
         }
 
         if(empty($phone)) {
-            $this->validationErrors[] = "Please write in your post code";
+            $this->validationErrors[] = "Please write in your phone number";
         }
 
         if (strlen($phone) != "8") {
