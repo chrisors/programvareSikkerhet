95c6c902a9b72c302baed53cb1880f8028ca8e76
Christian Soerseth <csorseth@gmail.com>
2016-10-29 13:49:26 +0200

    dunno

--- src/webapp/controllers/PatentsController.php
+++ src/webapp/controllers/PatentsController.php
@@ -44,7 +44,6 @@ class PatentsController extends Controller
       }else{
         $searchQuery = "";
       }
-//      $searchQuery = $this->patentRepository->searchPatents($company, $title);
 
       $this->render('patents/index.twig', [
           'patent' => $patent,
